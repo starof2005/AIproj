9f03e69efcc1dba153b15333859866985f559695
Sanjeev Santhosh <sanjeevsanthosh143@gmail.com>
2026-03-01 12:11:29 +0530

    fixed render port binding

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import os
+
+
 
 app = Flask(__name__)
 
@@ -30,4 +33,6 @@ def home():
 
     return render_template("index.html", result=result, hours=hours)
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
